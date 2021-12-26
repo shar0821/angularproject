3e18a7f9418584a2e05660ce58fc050f752d174a
Sushaanth <80441330+sushaanth01@users.noreply.github.com>
2021-12-26 18:56:11 +0530

    Added builderblock and variables for select component

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BuilderBlock } from '@builder.io/angular';
+
+@BuilderBlock({
+  tag: 'new-select',
+  name: 'new select',
+  inputs: [
+    {
+      name: 'selectlabel',
+      type: 'string',
+    },
+    {
+      name: 'selectoption1',
+      type: 'string',
+    },
+    {
+      name: 'selectoption2',
+      type: 'string',
+    },
+    {
+      name: 'selectoption3',
+      type: 'string',
+    },
+  ],
+})
 
 @Component({
   selector: 'app-select',
@@ -12,5 +36,10 @@ export class SelectComponent implements OnInit {
   ngOnInit(): void {
     
   }
-  
+
+  @Input() selectlabel = 'Select Label';
+  @Input() selectoption1 = 'Option 1';
+  @Input() selectoption2 = 'Option 2';
+  @Input() selectoption3 = 'Option 3';
+
 }
