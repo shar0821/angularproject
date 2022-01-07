695ba7848781c0ba856b5a8cb24c05debb69bc92
Sushaanth <80441330+sushaanth01@users.noreply.github.com>
2022-01-07 11:04:42 +0530

    Cleaned up code and comments

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -27,26 +27,17 @@ export class InputComponent implements OnInit {
 
   ngOnInit(): void {
     const x=document.querySelector('#I') as HTMLInputElement;
-      //x.addEventListener('keypress',()=>{this.display();});
       x.addEventListener('keyup',(e)=>{this.display(e);});
-  }
-  getInput() {
-      //document.addEventListener(input);
-      
   }
   display(e): void{
     e.preventDefault();
-     if(e.keyCode===13){//enter key thala
-       //console.log(this.value);//only when enter key is pressed
-       window.alert("input works!!!");
-       this.value="";
-      }
+    if(e.keyCode===13){ //enter key 
+      window.alert("input works!!!");
+      this.value="";
+    }
 
   }
-  /*display(): void
-  {
-    console.log(this.value);
-  }*/
+
   @Input() inputlabel = 'Input Label';
   @Input() inputplaceholder = 'Input Placeholder';
   @Input() value = '';
